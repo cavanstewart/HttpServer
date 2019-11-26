75c528e1264c95040a3aa340550d55f1cf609694
Cavan Stewart <cavanwstewart@gmail.com>
2019-11-26 15:52:31 -0800

    adds basic server functionality

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // write your code here
+const http = require('http');
+
+const server = http.createServer(function (req, res) {
+    
+    console.log('Received' + res.method + 'request for:' + req.url);
+
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+
+    res.end("Hello World");
+});
+
+
+
+
+server.listen(8080, 'localhost', null, function () {
+    console.log('Server is listening on port 8080')
+})
